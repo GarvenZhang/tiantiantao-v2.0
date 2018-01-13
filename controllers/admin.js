85a743220cc61e79f80f12351252ee64f01a5cb7
GarvenZhang <jf00258jf@hotmail.com>
2018-01-13 22:31:21 +0800

    admin page

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,17 +1,33 @@
+const cache = require('../middlewares/cache')
+/**
+ * 商品管理
+ */
 exports.goods = async ctx => {
-  ctx.render('/admin/index', {
-    data: []
+  ctx.render('/admin/goods', {
+    data: cache.getModel('goodsModel')
   })
 }
-
+/**
+ * 会员管理
+ */
 exports.vip = async ctx => {
-
+  ctx.render('/admin/vip', {
+    data: cache.getModel('userModel')
+  })
 }
-
+/**
+ * 类别管理
+ */
 exports.type = async ctx => {
-
+  ctx.render('/admin/category', {
+    data: cache.getModel('categoryModel')
+  })
 }
-
-exports.orderform = async ctx => {
-
+/**
+ * 订单管理
+ */
+exports.orderForm = async ctx => {
+  ctx.render('/admin/orderform', {
+    data: cache.getModel('orderFormModel')
+  })
 }
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const baseTips = require('../middlewares/baseTips')
 let Cache = require('../middlewares/cache')
 let mysqlModule = require('../middlewares/mysqlModule')
+let session = require('../middlewares/session')
 /**
  * 登陆
  */
@@ -8,8 +9,14 @@ exports.login = async ctx => {
   const postData = ctx.filteredData
   await mysqlModule.queryConnection('SELECT idUser FROM User WHERE account = ? AND password = ?', [postData.account, postData.password])
     .then(async result => {
-      await Cache.setDefaultModel('categoryModel')
-      ctx.resbody = Cache.getModel('categoryModel')
+      session.init(ctx.headers['cookie'], result, ctx.res)
+      // 管理员权限
+      if (ctx.account === 'admin') {
+        ctx.res.redirect('/admin/goods')
+      // 用户权限
+      } else {
+        ctx.res.redirect('/info/userInfo')
+      }
     })
     .catch(error => {
       console.log(`添加类别error：${error}`)
