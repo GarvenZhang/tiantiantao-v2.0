322196fc6d94c6be334084201a91b755f7f25dcf
GarvenZhang <jf00258jf@hotmail.com>
2018-01-13 14:51:28 +0800

    admin-goods

---
+++ controllers/admin.js
@@ -0,0 +1,17 @@
+exports.goods = async ctx => {
+  ctx.render('/admin/index', {
+    data: []
+  })
+}
+
+exports.vip = async ctx => {
+
+}
+
+exports.goods = async ctx => {
+
+}
+
+exports.goods = async ctx => {
+
+}
